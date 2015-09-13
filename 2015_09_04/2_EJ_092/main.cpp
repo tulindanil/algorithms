b41952f5374ce7cccfd30effd083912867c0ff35
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-09-13 19:27:40 +0000

    Inittial commit, main.cpp isn't ready for EJ yet
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@11 27ab2815-0c4e-2c49-80cd-d2551c38e382

---
+++ 2015_09_04/2_EJ_092/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2_EJ_092
+//
+//  Created by Danil Tulin on 9/13/15.
+//  Copyright (c) 2015 Danil Tulin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
