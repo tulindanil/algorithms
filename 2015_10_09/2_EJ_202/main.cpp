3f6a5de5f0c4096a6c124f64b255ed1c9f4a21e6
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-10-10 21:47:07 +0000

    2_EJ_202 is done
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@55 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- 2015_10_09/2_EJ_202/main.cpp
+++ 2015_10_09/2_EJ_202/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+#include <vector>
 
-size_t partitions(int n, int k)
+size_t partitions(int n, int k, std::vector<std::vector<int> > &matrix)
 {
+    if (matrix[n][k] != -1)
+        return matrix[n][k];
+    
     if (n == 0 && k == 0)
+    {
+        matrix[n][k] = 1;
         return 1;
+    }
     
     if (k == 0)
+    {
+        matrix[n][k] = 0;
         return 0;
+    }
     
     if (k <= n)
-        return partitions(n, k - 1) + partitions(n - k, k);
+    {
+        matrix[n][k] = partitions(n, k - 1, matrix) + partitions(n - k, k, matrix);
+        return matrix[n][k];
+    }
     else
-        return partitions(n, n);
+    {
+        matrix[n][k] = partitions(n, n, matrix);
+        return matrix[n][k];
+    }
 }
 
 int main()
@@ -19,6 +35,8 @@ int main()
     size_t price = 0;
     std::cin >> price;
     
-    std::cout << partitions(price, price);
+    std::vector<std::vector<int> > matrix = std::vector<std::vector<int> >(price + 1, std::vector<int>(price + 1, -1));
+    std::cout << partitions(price, price, matrix);
+    
     return 0;
 }
