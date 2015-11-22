fd48ed8d1ab15f3259a601e4e52cac9e7f42e81a
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-11-22 21:51:29 +0000

    EJ_105 solved
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@116 27ab2815-0c4e-2c49-80cd-d2551c38e382

---
+++ 2015_11_06/EJ_105/judje.cc
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <cmath>
+
+namespace dummy {
+
+using std::pow;
+
+char *logs;
+long long int *bins;
+
+int _log(int x) {
+    return logs[x] + 1;
+}
+
+int powered(int x) {
+    size_t value = 1;
+    while (x > 0) {
+        value <<= 1;
+        x--;
+    }
+    return value;
+}
+
+int pow(int x) {
+    return bins[x];
+}
+
+template<class T>
+class storage {
+
+public:
+
+    storage() {
+
+    }
+
+    storage(const std::vector<T> &v): m(matrix(_log(v.size()) + 1, row(v.size(), T()))) {
+        fill(v.size());
+        m.at(0) = v;
+        for (size_t k = 1; k <= _log(v.size()); ++k) {
+            for (int i = 0; i <= (int)v.size() - pow(k); ++i) {
+                try {
+                    m.at(k).at(i) = std::min(m.at(k - 1).at(i), m.at(k - 1).at(i + pow(k - 1)));
+                } catch (std::exception &) {
+                    std::cout << v.size() - pow(2, k) << " " << v.size() - dummy::pow(k)  << std::endl;
+                    abort();
+                }
+            }
+        }
+    }
+
+    T rmq(size_t begin, size_t end) const {
+        size_t length = end - begin;
+        int k = _log(length) - 1;
+        return std::min(m.at(k).at(begin), m.at(k).at(end - dummy::pow(k)));
+    }
+
+    static void fill(size_t size) {
+        logs = new char[size + 1]; 
+        int pos = 0;
+        int lim = 2;
+        int log = 0;
+
+        while(pos < size) {
+            while(pos < lim) {
+                logs[pos] = log;
+                pos++;
+            }
+
+            lim <<= 1;
+            log++;
+
+            if (lim > size)
+                lim = size;
+        }
+        logs[size] = logs[size - 1];
+
+        bins = new long long int[size];
+        for (size_t i = 0; i < size; ++i) {
+            bins[i] = pow(2, i);
+        }
+    }
+
+private:
+
+    typedef std::vector<T> row;
+    typedef std::vector<row> matrix;
+
+    matrix m;
+
+};
+}
+
+using dummy::storage;
+
+int main() {
+
+    std::cin.sync_with_stdio();
+    std::cout.sync_with_stdio();
+
+    size_t size = 0;
+    std::cin >> size;
+    std::vector<float> v(size, 0);
+
+    for (size_t i = 0; i < size; ++i) {
+        float value;
+        std::cin >> value;
+        v.at(i) = value;
+    }
+
+    storage<float> s = storage<float>(v);
+
+    std::cin >> size;
+    size_t fst, snd;
+    for (size_t i = 0; i < size; ++i) {
+        std::cin >> fst >> snd;
+        std::cout << s.rmq(fst, snd) << std::endl;
+    }
+
+    return 0;
+}
