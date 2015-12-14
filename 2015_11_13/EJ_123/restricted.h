36b3d6d147a836504a0c30bd92c93fb6ab6065db
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-14 10:50:59 +0000

    bug fixes
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@202 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- 2015_11_13/EJ_123/restricted.h
+++ 2015_11_13/EJ_123/restricted.h
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 #include <cassert>
-#include "dummy_storage.h"
 
 namespace restricted {
 
@@ -10,40 +9,27 @@ class storage {
 
 public:
 
+    typedef std::vector<T> sequence;
+
     storage(sequence& s): data(&s), block_size(log2(s.size) / 2), 
-                          offsets(positions(pow(2, block_size - 1), -1)) {
+                          pttrns(positions(pow(2, block_size - 1))) {
         s.resize(s.size() + s.size() % block_size);
         dummy_sequence raw = dummy_sequence(s.size() / block_size);
-        dummy::storage::hold(raw.size());
+        dummy::storage<dummy_t>::hold(raw.size());
         build_pttrns(raw);
         dummy = dummy::storage<dummy_t>(raw);
     }
 
-    inline std::pair(T, size_t) rmq(size_t l, size_t r) const {
+    inline std::pair<T, size_t> rmq(size_t l, size_t r) const {
         size_t l_blocked = (l + block_size - l % block_size) / block_size;
         size_t r_blocked = (r - r % block_size) / block_size;
         std::pair<T, size_t> block_min = dummy.rmq(l_blocked, r_blocked),
         min = std::min(block_min, dummy_rmq(l, l + block_size - l % block_size));
-        min = std::min(min, dummy_rmq(r_blocked, r);
-        return std::make_pair(min, pos);
+        min = std::min(min, dummy_rmq(r_blocked, r));
+        return min;
     }
 
 private:
-
-    typedef std::vector<T> sequence;
-    typedef std::vector<bool> bits;
-    typedef std::vector<block> pttrns;
-
-    typedef std::pair<T, size_t> dummy_t
-    typedef std::vector<dummy_t> dummy_sequence;
-    dummy::storage<dummy_t> dummy;
-
-    sequence* data;
-    sequence blocked_data;
-
-    positions offsets;
-    size_t block_size;
-
     struct block {
 
         block(): offset(-1) {
@@ -52,7 +38,21 @@ private:
 
         T value;
         int offset;
-    }
+    };
+
+    typedef std::vector<bool> bits;
+    typedef std::vector<size_t> positions;
+
+    typedef std::pair<T, size_t> dummy_t;
+    typedef std::vector<dummy_t> dummy_sequence;
+    dummy::storage<dummy_t> dummy;
+
+    sequence* data;
+    sequence blocked_data;
+
+    std::vector<block> pttrns;
+
+    size_t block_size;
 
     inline int getHash(const bits& mask) {
         int index = 0;
@@ -64,14 +64,14 @@ private:
         return index;
     }
 
-    inline bits getMask(const sequence::const_iterator& begin) {
-        bits mask = bits(period - 1);
-        for (typename sequenece::const_iterator it = begin; 
-             it != begin + period - 1; 
+    inline bits getMask(const typename sequence::const_iterator& begin) {
+        bits mask = bits(block_size - 1);
+        for (typename sequence::const_iterator it = begin; 
+             it != begin + block_size - 1; 
              ++it) {
             if (*(it + 1) - *it == 1) {
                 mask[it - begin] = true;
-            } else (*(it + 1) - *it == -1) {
+            } else if (*(it + 1) - *it == -1) {
                 mask[it - begin] = false;
             } else {
                 std::cerr << "not suitable array" << std::endl;
@@ -97,7 +97,7 @@ private:
     inline void buildNewPttrn(int& ofs, T& value, const bits& mask) {
         T min = value;
         for (typename bits::const_iterator it = mask.begin();
-                it != bits.end();
+                it != mask.end();
                 ++it) {
             if (*it == true) {
                 value++;
