c008accc446513e31906a4df495f1d03746bc461
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-14 10:51:00 +0000

    add operator <<
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@204 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- 2015_11_06/EJ_105/dummy_storage.h
+++ 2015_11_06/EJ_105/dummy_storage.h
@@ -41,20 +41,26 @@ public:
 
     storage(std::vector<T> &v): data(&v), m(matrix(_log(v.size()) + 1)) {
         size_t size = v.size();
+        m.at(0) = v;
         for (size_t k = 1; k <= _log(size); ++k) {
-            m.at(k) = row((int)size - pow(k) + pow(k - 1) + 1);
+            m.at(k) = row((int)size - pow(k - 1) + 1);
             for (int i = 0; i <= (int)size - pow(k); ++i) {
                 m.at(k).at(i) = std::min(at(k - 1, i), at(k - 1, i + pow(k - 1)));
-                }
             }
+        }
     }
 
     inline const T& at(size_t row, size_t column) const {
-        if (row == 0) {
-            assert(data != NULL);
-            return data->at(column);
+        try {
+            //if (row == 0) {
+//                if (data == NULL)
+//                    std::cout << "data is nil" << std::endl, abort();
+            //    return data->at(column);
+           // }
+            return m.at(row).at(column);
+        } catch (std::exception& e) {
+            abort();
         }
-        return m.at(row).at(column);
     }
 
     inline T rmq(size_t begin, size_t end) const {
@@ -85,6 +91,16 @@ public:
 
     const T& operator[](size_t index) {
         return m[0][index];
+    }
+
+    template<class V> friend
+    std::ostream& operator<<(std::ostream& os, const storage<V>& s) {
+        os << "dummy::storage" << std::endl;
+//        os << *(s.data) << std::endl;
+        for (typename storage<V>::matrix::const_iterator it = s.m.begin(); it != s.m.end(); ++it) {
+            os << *it << std::endl;
+        }
+        return os;
     }
 
 private:
