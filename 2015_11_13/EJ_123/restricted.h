a11892276b788a9fe5a47ada11e77d5a984a42d4
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-14 10:50:56 +0000

    add pttrns building
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@197 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- 2015_11_13/EJ_123/restricted.h
+++ 2015_11_13/EJ_123/restricted.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cmath>
 #include <cassert>
 #include "dummy_storage.h"
@@ -9,7 +10,8 @@ class storage {
 
 public:
 
-    storage(sequence& s): data(&s), block_size(log2(s.size) / 2) {
+    storage(sequence& s): data(&s), block_size(log2(s.size) / 2), 
+                          offsets(positions(pow(2, block_size - 1), -1)) {
         s.resize(s.size() + s.size() % block_size);
         dummy::storage::hold(s.size());
     }
@@ -27,13 +29,83 @@ private:
 
     typedef std::vector<T> sequence;
     typedef std::vector<bool> bits;
+    typedef std::vector<block> pttrns;
 
     typedef std::pair<T, size_t> dummy_t;
     dummy::storage<dummy_t> dummy;
 
     sequence* data;
+    sequence blocked_data;
+
+    positions offsets;
     size_t block_size;
 
+    struct block {
+
+        block(): offset(-1) {
+
+        }
+
+        T value;
+        int offset;
+    }
+
+    inline int getHash(const bits& mask) {
+        int index = 0;
+        for (typename bits::const_iterator it = mask.begin(); 
+             it != mask.end(); 
+             ++it) {
+            index <<= *it;
+        }
+        return index;
+    }
+
+    inline bits getMask(const sequence::const_iterator& begin) {
+        bits mask = bits(period - 1);
+        for (typename sequenece::const_iterator it = begin; 
+             it != begin + period - 1; 
+             ++it) {
+            if (*(it + 1) - *it == 1) {
+                mask[it - begin] = true;
+            } else (*(it + 1) - *it == -1) {
+                mask[it - begin] = false;
+            } else {
+                std::cerr << "not suitable array" << std::endl;
+                abort();
+            }
+        }
+    }
+
+    void build_pttrns() {
+        bits mask;
+        int hash = 0;
+        for (typename sequence::const_iterator it = data->begin();
+             it != data->end();
+             ++it) {
+            mask = getMask(it);
+            hash = getHash(mask);
+
+            if (pttrns[hash].offset == -1)
+                buildNewPttrn(pttrns[hash].offset, pttrns[hash].value,  mask);
+
+        }
+    }
+
+    inline void buildNewPttrn(int& ofs, T& value, const bits& mask) {
+        T min = value;
+        for (typename bits::const_iterator it = mask.begin();
+                it != bits.end();
+                ++it) {
+            if (*it == true) {
+                value++;
+           } else {
+                value--;
+                min = std::min(min, value);
+                if (value == min) 
+                    ofs = it - mask.begin();
+            }
+        }
+    }
 
     inline T dummy_rmq(size_t l, size_t r) const {
         assert(data != NULL);
