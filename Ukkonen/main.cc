07a280d4da8760f41f86f174b569b931e127e6cc
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-16 19:21:55 +0000

    update for problem solving
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@248 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- Ukkonen/main.cc
+++ Ukkonen/main.cc
@@ -2,15 +2,37 @@
 #include <cassert>
 #include <string>
 #include <map>
+#include <vector>
 #include "SuffixTree.h"
 
+class Solver {
+
+    public:
+
+
+       static void solve(const std::string& orig, const std::string& current) {
+//           Solver s(orig);
+       }
+
+
+    private:
+
+        SuffixTree tree;
+
+//        Solver(const std::string& orig): tree(orig) { }
+
+        std::vector<Result> proceed(const std::string& s) const {
+            std::vector<Result> results;
+           return results;
+        }
+
+};
+
 int main() {
-    int k;
-    std::string word;
-    std::cin >> word;
-    int length = word.length();
-    SuffixTree tree;
-    for (auto it: word) 
-        tree.push_back(it);
-    std::cout << tree.count() << std::endl;
+    std::string s1 = "I-love-basketball";
+    std::string s2 = "I-love-football";
+    SuffixTree tree(s1  + std::string("#") + s2);
+    std::cout << s1 << std::endl << s2 << std::endl;
+    tree.solve(std::cout);
+    return 0;
 }
