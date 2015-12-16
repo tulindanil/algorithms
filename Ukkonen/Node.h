e47d58079f4eba13c09f56599179a59b6fcd4303
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-16 19:21:53 +0000

    ad printing
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@246 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- Ukkonen/Node.h
+++ Ukkonen/Node.h
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 class Node {
 
 public:
@@ -5,6 +7,11 @@ public:
     int left, right;
     std::map<char, Node *> to;
     Node *parent, *link;
+    bool isLeaf;
+
+    int value;
+
+    int length;
 
     Node(int left, int right): 
         left(left), 
@@ -23,6 +30,40 @@ public:
         for (auto node: to)
             count += node.second->count(length);
         return count;
+    }
+
+    std::pair<int, int> proceed(const std::string& s, int divider) {
+        std::string content = s.substr(left, right - left);
+        int pos = content.find("#");
+        if (pos != -1) {
+            right = left + pos;
+        }
+        if (isLeaf) {
+            value = (left >= divider) ? 2 : 1;
+            return {value, 0}; 
+        }
+        value = 0;
+        length = 0;
+        for(auto node: to) {
+            auto p = node.second->proceed(s, divider);
+            if (p.first == 3) {
+                length = std::max(p.second + (right - left), length);
+            }
+            value |= p.first;
+        }
+        if (value == 3 && length == 0) {
+            length = right - left;
+        }
+        return {value, length};
+    }
+    
+    void print(std::ostream& os, const std::string& s, int offset = 0) const {
+        os << std::string(offset, '*'); 
+        std::string content = s.substr(left, right - left);
+        os << content << (isLeaf ? "(leaf) " : " ") << value << " " << length << std::endl; 
+        for (auto it: to) {
+            it.second->print(os, s, offset + content.length());
+        }
     }
 
 };
