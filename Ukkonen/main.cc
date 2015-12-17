4b5667d9761f74ac0d299c7edeee35dae197b229
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-17 23:56:34 +0000

    now I am reversing strings
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@308 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- Ukkonen/main.cc
+++ Ukkonen/main.cc
@@ -3,13 +3,32 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <list>
+
+struct Result {
+
+    size_t current;
+    size_t length;
+    size_t matching;
+
+    friend std::ostream& operator <<(std::ostream& os, const Result& r) {
+        os << r.current << " | " << r.length << " | " << r.matching;
+        return os;
+    }
+
+};
+
 #include "SuffixTree.h"
 
 int main() {
     std::string s1 = "I-love-basketball";
     std::string s2 = "I-love-football";
-    SuffixTree tree(s1  + std::string("#") + s2);
     std::cout << s1 << std::endl << s2 << std::endl;
-    tree.solve(std::cout, s1);
+
+    std::reverse(s1.begin(), s1.end());
+    std::reverse(s2.begin(), s2.end());
+
+    SuffixTree tree(s2);
+    tree.proceed(s1);
     return 0;
 }
