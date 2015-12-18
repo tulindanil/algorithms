176927ab7b3d8e9ccfee194e1e73c615af068b26
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-18 09:49:24 +0000

    add debug functions
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@318 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- 2015_11_13/restricted/restricted.h
+++ 2015_11_13/restricted/restricted.h
@@ -4,6 +4,22 @@
 #include <cassert>
 #include <vector>
 
+class Debug {
+
+public:
+
+    template<class T>
+    friend std::ostream& operator <<(Debug& d, const T& object) {
+#ifdef DEBUG
+        std::cout << object;
+#endif
+        return std::cout;
+    }
+
+};
+
+Debug debug;
+
 namespace restricted {
 
 template<class T>
@@ -15,25 +31,39 @@ public:
 
     storage() { }
 
-    storage(sequence& s): data(&s), 
+    storage(sequence& s): data(s), 
                           block_size(log2(s.size()) / 2 > 2 ? log2(s.size()) / 2 : 2), 
                           pttrns(std::vector<block>(pow(2, block_size - 1))) {
-        s.resize(s.size() + s.size() % block_size, s.back());
-        raw = dummy_sequence(s.size() / block_size);
+        data.resize(s.size() + ((s.size() % block_size) ? (block_size - s.size() % block_size) : 0), s.back());
+        raw = dummy_sequence((s.size() / block_size) + 1);
         dummy::storage<dummy_t>::fill(raw.size());
         build_pttrns();
         dummy = dummy::storage<dummy_t>(raw);
     }
 
-    inline std::pair<T, size_t> rmq(size_t l, size_t r) const {
-        if (r - l >= block_size) {
-            int l_blocked = (l - 1 - ((l - 1) % block_size)) / block_size + 1;
-            int r_blocked = (r + 1 - ((r + 1) % block_size)) / block_size;
+    inline std::pair<T, size_t> rmq(int l, int r) const {
+#ifdef DEBUG
+        debug << "rmq query " << l << " " << r << std::endl; 
+#endif
+        if (r - l > block_size) {
+            int l_blocked = 0;
+            if (l == 0)
+                l_blocked = 0;
+            else
+                l_blocked = (l - 1 - ((l - 1) % block_size)) / block_size + 1;
+            int r_blocked = (r - ((r) % block_size)) / block_size;
+#ifdef DEBUG
+            debug << "l_blocked: " << l_blocked << std::endl;
+            debug << "r_blocked: " << r_blocked << std::endl;
+#endif
             std::pair<T, size_t> min = dummy.rmq(l_blocked, r_blocked);
             if (l != l_blocked * block_size)
                  min = std::min(min, dummy_rmq(l, l_blocked * block_size));
             if (r_blocked * block_size - 1 != r)
                 min = std::min(min, dummy_rmq(r_blocked * block_size - 1, r));
+#ifdef DEBUG
+            debug << "min is " << min << std::endl;
+#endif
             return min;
         } else {
             return dummy_rmq(l, r);
@@ -72,7 +102,7 @@ private:
     typedef std::vector<dummy_t> dummy_sequence;
     dummy::storage<dummy_t> dummy;
 
-    sequence* data;
+    sequence data;
 
     dummy_sequence raw; 
 
@@ -107,14 +137,23 @@ private:
     void build_pttrns() {
         bits mask;
         int hash = 0;
-        for (typename sequence::const_iterator it = data->begin();
-             it != data->end();
-             it += block_size) {
+        for (typename sequence::const_iterator it = data.begin();
+            it != data.end();
+            it += block_size) {
             mask = getMask(it);
             hash = getHash(mask);
             if (pttrns[hash].offset == -1)
                 buildNewPttrn(pttrns[hash].offset, mask, it);
-            raw[(it - data->begin()) / block_size] = std::make_pair(*(it + pttrns[hash].offset), (it - data->begin()) + pttrns[hash].offset);
+            try {
+                raw.at((it - data.begin()) / block_size) = std::make_pair(*(it + pttrns[hash].offset), (it - data.begin()) + pttrns[hash].offset);
+            } catch (std::exception& e) {
+#ifdef DEBUG
+                std::cout << *this << std::endl;
+                std::cout << "data size: " << data.size() << std::endl; 
+                std::cout << "exception at raw " << (it - data.begin()) / block_size << std::endl;
+#endif 
+                abort();
+            }
         }
     }
 
@@ -136,10 +175,9 @@ private:
     }
 
     inline std::pair<T, size_t> dummy_rmq(size_t l, size_t r) const {
-        assert(data != NULL);
-        std::pair<T, long> min = std::make_pair(data->at(l), l);
-        for (typename sequence::const_iterator it = data->begin() + l; it != data->begin() + r; ++it) {
-            min = std::min(min, std::make_pair(*it, (long)(it - (data->begin() + l))));
+        std::pair<T, long> min = std::make_pair(data.at(l), l);
+        for (typename sequence::const_iterator it = data.begin() + l; it != data.begin() + r; ++it) {
+            min = std::min(min, std::make_pair(*it, (long)(it - (data.begin() + l))));
         }
         return min;
     }
