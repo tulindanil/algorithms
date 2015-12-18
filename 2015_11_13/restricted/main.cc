968895c8c1d489d1b4447c1c6067ae705a289e6b
Тулин Даниил < @27ab2815-0c4e-2c49-80cd-d2551c38e382>
2015-12-18 09:49:24 +0000

    add ifdef to main.cc
    git-svn-id: svn://91.192.68.193:3072/497/%D0%A2%D1%83%D0%BB%D0%B8%D0%BD%20%D0%94%D0%B0%D0%BD%D0%B8%D0%B8%D0%BB@317 27ab2815-0c4e-2c49-80cd-d2551c38e382

--- 2015_11_13/restricted/main.cc
+++ 2015_11_13/restricted/main.cc
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <vector>
+
+#ifdef DEBUG
+
+template<class V, class K>
+std::ostream& operator<<(std::ostream& os, const std::pair<K, V>& p) {
+    os << "{" << p.first << ", " << p.second << "}";
+    return os;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    if (v.size() == 0)
+        return os;
+    os << "[";
+    for (typename std::vector<T>::const_iterator it = v.begin(); it != v.end(); ++it) {
+        bool isLast = (it == v.end() - 1);
+        os << *it << (isLast ? "]" : ", "); 
+    }
+    os << " " << v.size();
+    return os;
+}
+
+#endif 
+
 #include "dummy_storage.h"
 #include "restricted.h"
 
@@ -20,6 +45,10 @@ int main() {
     }
 
     storage<float> s = storage<float>(v);
+
+#ifdef DEBUG
+    std::cout << s << std::endl;
+#endif 
 
     std::cin >> size;
     int fst, snd;
